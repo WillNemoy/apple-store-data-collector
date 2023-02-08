6b6de387fc06885a50f7a0e225f7463b4e8b55f1
Will Nemoy <98560701+WillNemoy@users.noreply.github.com>
2023-02-08 15:41:40 -0500

    Fixed error when less than 500 reviews

--- app.py
+++ app.py
@@ -14,49 +14,52 @@ def appleAPI(appId):
     dfs = []
 
     for i in range(10):
-        page = i + 1
-        
-        apple_request = ("https://itunes.apple.com/us/rss/customerreviews/page="
-                        + str(page)
-                        + "/id="
-                        + appId
-                        + "/sortBy=mostHelpful/json")
+        try:
+            page = i + 1
+            
+            apple_request = ("https://itunes.apple.com/us/rss/customerreviews/page="
+                            + str(page)
+                            + "/id="
+                            + appId
+                            + "/sortBy=mostHelpful/json")
 
-        apple_Json = requests.get(apple_request).json()
+            apple_Json = requests.get(apple_request).json()
 
-        df = pd.DataFrame(apple_Json["feed"]["entry"])
+            df = pd.DataFrame(apple_Json["feed"]["entry"])
 
-        #fix up the df (convert json to content)
-        df["author"] = df["author"].apply(lambda x: x["name"]["label"])
-        df["updated"] = df["updated"].apply(lambda x: x["label"])
-        df["im:rating"] = df["im:rating"].apply(lambda x: x["label"])
-        df = df.rename(columns={"im:rating": "rating"})
+            #fix up the df (convert json to content)
+            df["author"] = df["author"].apply(lambda x: x["name"]["label"])
+            df["updated"] = df["updated"].apply(lambda x: x["label"])
+            df["im:rating"] = df["im:rating"].apply(lambda x: x["label"])
+            df = df.rename(columns={"im:rating": "rating"})
 
-        df["im:version"] = df["im:version"].apply(lambda x: x["label"])
-        df = df.rename(columns={"im:version": "version"})
+            df["im:version"] = df["im:version"].apply(lambda x: x["label"])
+            df = df.rename(columns={"im:version": "version"})
 
-        df["id"] = df["id"].apply(lambda x: x["label"])
-        df["title"] = df["title"].apply(lambda x: x["label"])
-        df["content"] = df["content"].apply(lambda x: x["label"])
+            df["id"] = df["id"].apply(lambda x: x["label"])
+            df["title"] = df["title"].apply(lambda x: x["label"])
+            df["content"] = df["content"].apply(lambda x: x["label"])
 
-        df["link"] = df["link"].apply(lambda x: x["attributes"]["href"])
-        df["im:voteSum"] = df["im:voteSum"].apply(lambda x: x["label"])
-        df = df.rename(columns={"im:voteSum": "voteSum"})
+            df["link"] = df["link"].apply(lambda x: x["attributes"]["href"])
+            df["im:voteSum"] = df["im:voteSum"].apply(lambda x: x["label"])
+            df = df.rename(columns={"im:voteSum": "voteSum"})
 
-        df["im:contentType"] = df["im:contentType"].apply(lambda x: x["attributes"]["label"])
-        df = df.rename(columns={"im:contentType": "contentType"})
+            df["im:contentType"] = df["im:contentType"].apply(lambda x: x["attributes"]["label"])
+            df = df.rename(columns={"im:contentType": "contentType"})
 
-        df["im:voteCount"] = df["im:voteCount"].apply(lambda x: x["label"])
-        df = df.rename(columns={"im:voteCount": "voteCount"})
+            df["im:voteCount"] = df["im:voteCount"].apply(lambda x: x["label"])
+            df = df.rename(columns={"im:voteCount": "voteCount"})
 
 
-        #create a day and time column
-        df["updated day"] = df["updated"].apply(lambda x: x.split("T")[0])
-        df["updated time"] = df["updated"].apply(lambda x: "T" + x.split("T")[1])
+            #create a day and time column
+            df["updated day"] = df["updated"].apply(lambda x: x.split("T")[0])
+            df["updated time"] = df["updated"].apply(lambda x: "T" + x.split("T")[1])
 
-        df = df.drop("updated", axis=1)
+            df = df.drop("updated", axis=1)
 
-        dfs.append(df)
+            dfs.append(df)
+        except:
+            break
 
     df_all = pd.concat(dfs).reset_index()
     df_all = df_all.drop("index", axis=1)
